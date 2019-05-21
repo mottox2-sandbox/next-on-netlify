ad3c4ab2f40435785fbd3622652173b4d3576e9f
mottox2 <y.mottox2@gmail.com>
2019-05-21 23:30:44 +0900

    Add Cache-Control header

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,11 @@ const Home = () => {
   return <div>Welcome to Next.js!</div>;
 }
 
+Home.getInitialProps = async props => {
+  if (props.res) {
+    props.res.setHeader('Cache-Control', 'public, s-maxage=30, stale-while-revalidate')
+  }
+  return {}
+}
+
 export default Home;
