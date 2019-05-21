3d54f834a32fc4500c1aa4499369c9e3e95546a9
mottox2 <y.mottox2@gmail.com>
2019-05-21 23:39:57 +0900

    Fix

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ const Home = () => {
   return <div>Welcome to Next.js!</div>;
 }
 
-Home.getInitialProps = async props => {
+Home.getInitialProps = props => {
+  console.log(props.res)
   if (props.res) {
+    console.log('setHeader')
     props.res.setHeader('Cache-Control', 'public, s-maxage=30, stale-while-revalidate')
   }
   return {}
